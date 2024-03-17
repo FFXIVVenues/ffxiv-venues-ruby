301a974a204b33a23f864c5dce010c74e52a681f
Andy James <andrewj@ksharp.net>
2024-03-17 15:24:58 +0000

    Add auto-threading and pinning functionalities

--- main.rb
+++ main.rb
@@ -9,7 +9,6 @@ Dotenv.overload ".env.local"
 @discord_token = ENV['DISCORD_TOKEN']
 @discord_client_id = ENV['DISCORD_CLIENT_ID']
 
-channels = {}
 
 bot = Discordrb::Bot.new(
   token: @discord_token,
@@ -18,6 +17,8 @@ bot = Discordrb::Bot.new(
 
 
 # AutoThreading
+channels_to_thread = {}
+
 bot.register_application_command :autothread,
                              "Enabled creating a thread on each message posted in this channel." do
   |interaction|
@@ -29,35 +30,65 @@ bot.application_command :autothread do
   Discordrb::LOGGER.info "Executing application command 'autothread' executing"
   channel_id = command.channel_id
   title = command.options["thread_name"]
-  new = channels[channel_id].nil?
+  new = channels_to_thread[channel_id].nil?
   if new
-    channels[channel_id] = title
+    channels_to_thread[channel_id] = title
     command.respond content: "I'll start automatically creating threads in this channel. 🥰", ephemeral: true
   else
-    channels.delete channel_id
+    channels_to_thread.delete channel_id
     command.respond content: "Okay, I won't create threads here anymore. 🥲", ephemeral: true
   end
 end
+
+bot.message do |event|
+  channel_id = event.channel.id
+  title = channels_to_thread[channel_id]
+  return unless title
+
+  event.channel.start_thread title, 10080, message: event.message
+end
 # End AutoThreading
 
+
+
 # Begin Pinning
+channels_to_anchor={}
+previous_anchors={}
+
 bot.register_application_command :anchor, "Enable anchoring a notice to the bottom of this channel." do
   |interaction|
   interaction.string "anchor_content", "The text content of the anchored message."
 end
 
 bot.application_command :anchor do
-  #todo
+  Discordrb::LOGGER.info "Executing application command 'anchor' executing"
+  channel_id = command.channel_id
+  content = command.options["anchor_content"]
+  new = channels_to_anchor[channel_id].nil?
+  if new
+    channels_to_anchor[channel_id] = content
+    command.respond content: "Oki, I'll anchor that message here! 🥰", ephemeral: true
+  else
+    channels_to_anchor.delete channel_id
+    command.respond content: "Okay, I won't anchor that message anymore. 🥲", ephemeral: true
+  end
 end
-# End Pinning
 
 bot.message do |event|
   channel_id = event.channel.id
-  title = channels[channel_id]
-  return unless title
+  content = channels_to_anchor[channel_id]
+  return unless content
 
-  event.channel.start_thread title, 10080, message: event.message
+  previous_anchor_id = previous_anchors[channel_id]
+  unless previous_anchor_id.nil?
+    previous_anchor = event.channel.get_message previous_anchor_id
+    event.channel.delete_message previous_anchor
+
+  event.channel.send_message "", embed: {}
 end
+# End Pinning
+
+
 
 bot.ready do
   Discordrb::LOGGER.info "Ruby is online!"
