1b0df7a86d06ebd07fb73d52cfb051825c01168a
Andy James <andrewj@ksharp.net>
2024-04-23 23:39:10 +0100

    Do not allow input less than 2

--- random.rb
+++ random.rb
@@ -18,7 +18,12 @@ class Random
     max_number = event.options["max_number"]
     max_inputted = !max_number.nil?
     max_number = 999 unless max_inputted
-    random_number = rand(0..max_number)
+    if max_number < 2
+      event.respond content: "\"#{max_number}\" is not a valid setting.", ephemeral: true
+      return
+    end
+
+    random_number = rand(1..max_number)
     event.respond content: "Random! #{event.user.nickname || event.user.global_name} rolls a 🎲#{random_number} (out of #{max_number})." if (max_inputted)
     event.respond content: "Random! #{event.user.nickname || event.user.global_name} rolls a 🎲#{random_number}." unless (max_inputted)
   end
