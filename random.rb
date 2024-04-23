7618d5babdb70a33b6aca1d0f5c590e7d089190f
Andy James <andrewj@ksharp.net>
2024-04-23 23:53:44 +0100

    Make the /random command (and now /dice command) more accurate to in game behaviour and descriptions

--- random.rb
+++ random.rb
@@ -6,10 +6,14 @@ class Random
   def initialize(bot)
     @bot = bot
 
-    @bot.register_application_command :random, "Roll a random number up to the number given (999 max by default).", default_permission: false do |interaction|
-      interaction.integer "max_number", "The text content of the anchored message.", required: false
+    @bot.register_application_command :random, "Displays a random number between 0 and 999 in the current chat channel.", default_permission: false do |interaction|
+      interaction.integer "upper_limit", "Set an upper limit to display a random number between 1 and specified number.", required: false
+    end
+    @bot.register_application_command :dice, "Displays a random number between 0 and 999 in the current chat channel.", default_permission: false do |interaction|
+      interaction.integer "upper_limit", "Set an upper limit to display a random number between 1 and specified number.", required: false
     end
     @bot.application_command :random, &method(:on_command)
+    @bot.application_command :dice, &method(:on_command)
   end
 
   def on_command(event)
@@ -17,13 +21,14 @@ class Random
 
     max_number = event.options["max_number"]
     max_inputted = !max_number.nil?
+    min_number = max_inputted ? 1 : 0
     max_number = 999 unless max_inputted
     if max_number < 2
       event.respond content: "\"#{max_number}\" is not a valid setting.", ephemeral: true
       return
     end
 
-    random_number = rand(1..max_number)
+    random_number = rand(min_number..max_number)
     event.respond content: "Random! #{event.user.nickname || event.user.global_name} rolls a 🎲#{random_number} (out of #{max_number})." if (max_inputted)
     event.respond content: "Random! #{event.user.nickname || event.user.global_name} rolls a 🎲#{random_number}." unless (max_inputted)
   end
